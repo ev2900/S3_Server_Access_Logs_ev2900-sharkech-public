1c223ac025ac8c20af7176eef6727606942a6392
Christopher Sharkey <ev2900@gmail.com>
2023-06-02 09:42:48 -0400

    multiple updatee

---
+++ 3_update_download_readme_icons.py
@@ -0,0 +1,51 @@
+import os 
+import re
+
+from datetime import datetime
+
+#
+# OpenSearch_CloudWatch_Alarms
+#
+
+with open("./Server_Access_Log_Data/" + datetime.today().strftime('%Y-%m-%d') + ".csv") as file:
+	server_access_log_agg_results = [line.rstrip() for line in file]
+file.close()
+
+# Get the number of GET OBJECTs for the repository
+for line in server_access_log_agg_results:
+
+	regex = r"misc-public/OpenSearch_cloudwatch_alarms.yaml.*"
+		
+	if re.match(regex, line):
+		split_line = line.split(",")
+		openSearch_cloudWatch_alarms_downloads = split_line[1]
+
+# Git pull on the OpenSearch_CloudWatch_Alarms repo
+os.system("git -C C:\\Users\\sharkech\\Desktop\\GitHub\\OpenSearch_CloudWatch_Alarms pull")
+
+# Open the README for OpenSearch_CloudWatch_Alarms
+with open("C:/Users/sharkech/Desktop/GitHub/OpenSearch_CloudWatch_Alarms/README.md") as README_file:
+	README_lines = [line.rstrip() for line in README_file]
+README_file.close()
+
+new_README_lines = []
+
+for line in README_lines:
+
+	regex = r'<img width="100" alt="map-user" src="https://img.shields.io/badge/downloads-.*'
+
+	if re.match(regex, line):
+		new_README_lines.append('<img width="100" alt="map-user" src="https://img.shields.io/badge/downloads-' + str(openSearch_cloudWatch_alarms_downloads) + '-blue">')
+	else:
+		new_README_lines.append(line)
+
+new_README_file = open("C:/Users/sharkech/Desktop/GitHub/OpenSearch_CloudWatch_Alarms/README.md", "w")
+
+for line in new_README_lines:
+	new_README_file.write(line + "\n")
+
+new_README_file.close()
+
+os.system("git -C C:\\Users\\sharkech\\Desktop\\GitHub\\OpenSearch_CloudWatch_Alarms add .")
+os.system('git -C C:\\Users\\sharkech\\Desktop\\GitHub\\OpenSearch_CloudWatch_Alarms commit -m "Updating downloads"')
+os.system("git -C C:\\Users\\sharkech\\Desktop\\GitHub\\OpenSearch_CloudWatch_Alarms push")
--- schedulable_script.ps1
+++ schedulable_script.ps1
@@ -1,3 +1,5 @@
-python C:\Users\sharkech\Desktop\GitHub\S3_Server_Access_Logs_ev2900-sharkech-public\aggregate_server_access_logs.py
+python C:\Users\sharkech\Desktop\GitHub\S3_Server_Access_Logs_ev2900-sharkech-public\1_aggregate_server_access_logs.py
 
-python C:\Users\sharkech\Desktop\GitHub\S3_Server_Access_Logs_ev2900-sharkech-public\analyze_server_access_logs.py
+python C:\Users\sharkech\Desktop\GitHub\S3_Server_Access_Logs_ev2900-sharkech-public\2_analyze_server_access_logs.py
+
+python C:\Users\sharkech\Desktop\GitHub\S3_Server_Access_Logs_ev2900-sharkech-public\3_update_download_readme_icons.py
