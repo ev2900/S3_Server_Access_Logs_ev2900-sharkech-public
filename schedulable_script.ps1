efed08b8f529a4bab472f2f09ca0746419eb77c5
Christopher Sharkey <ev2900@gmail.com>
2023-06-14 16:24:19 -0400

    update

--- schedulable_script.ps1
+++
@@ -1,5 +0,0 @@
-python C:\Users\sharkech\Desktop\GitHub\S3_Server_Access_Logs_ev2900-sharkech-public\1_aggregate_server_access_logs.py
-
-python C:\Users\sharkech\Desktop\GitHub\S3_Server_Access_Logs_ev2900-sharkech-public\2_analyze_server_access_logs.py
-
-python C:\Users\sharkech\Desktop\GitHub\S3_Server_Access_Logs_ev2900-sharkech-public\3_update_download_readme_icons.py
