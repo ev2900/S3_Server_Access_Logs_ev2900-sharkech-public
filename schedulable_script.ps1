59e67b9e58347b462c58e9d490c11f209241bbae
Christopher Sharkey <ev2900@gmail.com>
2023-09-23 10:29:18 -0400

    updates powershell script

---
+++ schedulable_script.ps1
@@ -0,0 +1,11 @@
+Write-Host "-- Running Script 1"
+
+python 1_aggregate_server_access_logs.py
+
+Write-Host "-- Running Script 2"
+
+python 2_analyze_server_access_logs.py
+
+Write-Host "-- Running Script 3"
+
+python 3_update_download_readme_icons.py
